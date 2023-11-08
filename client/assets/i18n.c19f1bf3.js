2cf982cdb1571759fdf7451789c10dce20e3b002
KonstantinSerebryakov <KonstantinSerebryakov2000@gmail.com>
2023-11-08 14:27:00 +0000

    Update from https://github.com/KonstantinSerebryakov/pyshop-client/commit/67d817243834bdce91ba23a3cba7d08efadd6559

--- client/assets/i18n.c19f1bf3.js
+++ client/assets/i18n.c19f1bf3.js
@@ -1,4 +1,4 @@
-import{d as mt,h as Zt,e as On,i as vn,o as kn,a as yn,s as Cn,r as ae,c as re,b as An,w as ze,F as zt,g as je,f as Pn,j as Rn,T as Sn,k as Fn}from"./index.cc854539.js";/*!
+import{d as mt,h as Zt,e as On,i as vn,o as kn,a as yn,s as Cn,r as ae,c as re,b as An,w as ze,F as zt,g as je,f as Pn,j as Rn,T as Sn,k as Fn}from"./index.baa58f94.js";/*!
   * shared v9.5.0
   * (c) 2023 kazuya kawaguchi
   * Released under the MIT License.
